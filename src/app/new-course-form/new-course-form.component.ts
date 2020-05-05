a096f59e21e5a5c20bc8886512687b10e49b384e
Sachin <sachin.2019@tecorb.co>
2020-05-06 02:08:35 +0530

    FormArray is implemented

--- src/app/new-course-form/new-course-form.component.ts
+++ src/app/new-course-form/new-course-form.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormArray } from '@angular/forms';
 
 @Component({
-  selector: 'app-new-course-form',
+  selector: 'new-course-form',
   templateUrl: './new-course-form.component.html',
   styleUrls: ['./new-course-form.component.css']
 })
-export class NewCourseFormComponent implements OnInit {
+export class NewCourseFormComponent  {
 
-  constructor() { }
+  form = new FormGroup({
+      topics: new FormArray([])
+  })
 
-  ngOnInit(): void {
+  addTopic(topic: HTMLInputElement){
+    (this.topics.push(new FormControl(topic.value)));
+    topic.value = ''
   }
-
+  get topics(){
+    return this.form.get('topics') as FormArray
+  }
+  removeTopic(topic: FormControl){
+    let index = this.topics.controls.indexOf(topic)
+    this.topics.removeAt(index);
+  }
 }
