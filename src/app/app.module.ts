8c2d17883e0c230d9dfc798827a0900a601800e2
Sachin <sachin.2019@tecorb.co>
2020-05-05 12:21:27 +0530

    create simple forms with validation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { InputFormatDirective } from './input-format.directive';
 import { ZippyComponent } from './zippy/zippy.component';
+import { ContactFormComponent } from './contact-form/contact-form.component';
+import { from } from 'rxjs';
 
 @NgModule({
   declarations: [
     AppComponent,
     InputFormatDirective,
-    ZippyComponent
+    ZippyComponent,
+    ContactFormComponent
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/contact-form/contact-form.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'contact-form',
+  templateUrl: './contact-form.component.html',
+  styleUrls: ['./contact-form.component.css']
+})
+export class ContactFormComponent implements OnInit {
+ firstName: string;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  log(firstName){
+    console.log(firstName)
+  }
+
+}
